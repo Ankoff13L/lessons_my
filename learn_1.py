56dd62661b4817d941b7af7dd7ef44aad89f9a55
Andrei Kuznecov <ankoff13.com>
2023-10-12 10:39:13 +0300

    first commit

---
+++ learn_1.py
@@ -0,0 +1,21 @@
+# my_list = [4, 8, 15, 16, 23, 42, 'learn', 'python']
+# my_tuple = (4, 8, 15, 16, 23, 42, 'learn', 'python')
+
+# dir(my_list)
+
+
+def test(x, y):
+    return x + y
+
+# if __name__ == "__main__":
+#     res = test(1, 2)
+#     print(f"test: {res} | __name__: {__name__}")
+
+
+def main():
+    res = test(1, 2)
+    print(f"test: {res} | __name__: {__name__}")
+
+if __name__ == "__main__":
+    main()
+
