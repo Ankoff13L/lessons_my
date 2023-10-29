64d45486a792a031ca84136ad9e209296f86da78
Andrei Kuznecov <ankoff13@gmail.com>
2023-10-30 00:54:07 +0300

    continues

--- get_reguests.py
+++ get_reguests.py
@@ -21,8 +21,37 @@ import requests
 
 # print(response)
 
-payload = {'key1': 'value1', 'key2': ['value2', 'value3']}
-response = requests.get("https://httpbin.org/get", params=payload)
+# payload = {'key1': 'value1', 'key2': ['value2', 'value3']}
+# response = requests.get("https://httpbin.org/get", params=payload)
 
-print(response.url)
+# response.text
+# print(response.url)
+
+# url = 'https://httpbin.org/get'
+# url = 'https://api.github.com/events'
+
+# response = requests.get(url)
+# print(response.encoding)
+# response.encoding = 'ISO-8859-1'
+
+# print(response.encoding)
+# print(response.content)
+# print(response.text)
+# print(response.json)
+# r = response.json
+
+# print(response.raise_for_status)
+# print(response.status_code)
+
+# response = requests.get('https://api.github.com', stream=True)
+# response.raw
+# # response.raw.read(10)
+# print(response)
+
+url = 'https://api.github.com/some/endpoint'
+headers = {'user-agent': 'my-app/0.0.1'}
+r = requests.get(url, headers=headers)
+
+print(r)
+
 
