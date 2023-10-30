641971ec251fe53c97ad1e8359c08585fc83778d
Andrei Kuznecov <ankoff13@gmail.com>
2023-10-31 00:29:20 +0300

    continues

--- get_reguests.py
+++ get_reguests.py
@@ -48,10 +48,11 @@ import requests
 # # response.raw.read(10)
 # print(response)
 
-url = 'https://api.github.com/some/endpoint'
-headers = {'user-agent': 'my-app/0.0.1'}
-r = requests.get(url, headers=headers)
+# url = 'https://api.github.com/some/endpoint'
+# headers = {'user-agent': 'my-app/0.0.1'}
+# r = requests.get(url, headers=headers)
 
-print(r)
+# print(r)
+
 
 
