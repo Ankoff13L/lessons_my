1281f40dc5583cffac43ced53daef1614af5e46c
Andrei Kuznecov <ankoff13@gmail.com>
2023-11-13 00:30:29 +0300

    continues

---
+++ learn_2.py
@@ -0,0 +1,30 @@
+# a = None
+# b = 0
+
+# print(a is None)
+# print(b is None)
+# print(a is b)
+
+# c = [1, 2, 3]
+# d = [1, 2, 3]
+# print(c == d)
+# print(c is d)
+
+# c = [1,2,3]
+# d = c
+# print(c is d)
+
+a = 2
+print(type(a))
+
+b  = 2.0
+print(type(b))
+
+c = '2'
+print(type(c))
+
+d = '2.0'
+print(type(d))
+
+e = None
+print(type(e))
