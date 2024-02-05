1163deb0c68d5bf99465306de5e863ca31b68889
Andrei Kuznecov <ankoff13.com>
2024-02-05 18:52:30 +0300

    lesson python2day

--- lessons_bs4/lesson_1.py
+++ lessons_bs4/lesson_1.py
@@ -1,42 +1,29 @@
+# pip3 install beautifulsoup4 lxml
 from bs4 import BeautifulSoup
 
-"""Передача открытого объекта файла"""
-with open("index.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
+# """Передача открытого объекта файла"""
+# with open("index.html") as fp:
+#     soup = BeautifulSoup(fp, 'html.parser')
 
-"""Передача строки"""
-soup = BeautifulSoup("<html>a web page</html>", 'html.parser')
-
-
+# """Передача строки"""
+# soup = BeautifulSoup("<html>a web page</html>", 'html.parser')
 
 
+# # html_doc = """<html><head><title>The Dormouse's story</title></head>
+# <body>
+# <p class="title"><b>The Dormouse's story</b></p>
+
+# <p class="story">Once upon a time there were three little sisters; and their names were
+# <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+# <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+# <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+# and they lived at the bottom of a well.</p>
+
+# <p class="story">...</p>
+# """
 
 
-
-
-
-
-
-
-
-
-
-
-html_doc = """<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
-
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
-
-<p class="story">...</p>
-"""
-
-
-soup = BeautifulSoup(html_doc, 'html.parser')
+# soup = BeautifulSoup(html_doc, 'html.parser')
 
 # print(soup.prettify())
 
@@ -61,14 +48,29 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 #     print(link.get('href'))
 
 """Извлесь весь текст со страницы"""
-print(soup.get_text())
+# print(soup.get_text())
 
 
+"""Из Python2day"""
 
 
+with open("index.html") as file:
+    src = file.read()
 
+# print(src)
 
+soup = BeautifulSoup(src, "lxml")
 
+# title = soup.title
+# print(title)
+# print(title.text)
+# print(title.string)k
+
+"методы find() и find_all()"
+
+page_h1 = soup.find("h1")
+print(page_h1)
+print(page_h1.text)
 
 
 
