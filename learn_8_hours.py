971c52b5095408cd1010ca926a3bc2feb95d317c
Andrei Kuznecov <ankoff13@gmail.com>
2025-03-31 08:18:35 +0300

    New learn_8_hours

---
+++ learn_8_hours.py
@@ -0,0 +1,19 @@
+while True:
+    try:
+        num = int(input('Введите целое число: '))
+        break
+    except ValueError:
+        print('ERROR!!! Вы ввели неправильный формат!')
+print('Вы ввели:', num)
+
+
+
+
+
+
+# if 1:
+#     print('YES')
+# else:
+#     print('NO')
+
+# print('hello')
